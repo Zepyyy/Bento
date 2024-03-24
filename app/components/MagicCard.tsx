57551b4c16866766701887459674094c37b8e2c4
Quentin <quentin.stubecki@viacesi.fr>
2024-03-24 11:36:01 +0100

    colors

--- app/components/MagicCard.tsx
+++ app/components/MagicCard.tsx
@@ -1,11 +1,33 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { ColorThemeContext } from "@/app/contexts/ColorThemeContext";
 import { FaMagic } from "react-icons/fa";
 
 export default function MagicCard() {
 	const [colorTheme, setColorTheme] = useContext(ColorThemeContext);
-	const colors = ["Mirage", "WildBlueYonder", "WaterLeaf", "LavenderPurple"];
+	// color themes
+	const colors = [
+		"Mirage",
+		"WildBlueYonder",
+		"WaterLeaf",
+		"LavenderPurple",
+		"CoastalCalm",
+		"RusticCanvas",
+		"SereneOasis",
+		"UrbanTwilight",
+	];
 
+	// switch to a color theme after a random time interval between 1 and 2 seconds
+	useEffect(() => {
+		const interval = setInterval(
+			() => {
+				handleMagicClick();
+			},
+			Math.floor(Math.random() * 1000) + 1000,
+		);
+		return () => clearInterval(interval);
+	}, []);
+
+	// function to handle the magic click
 	function handleMagicClick() {
 		let newColorTheme = colorTheme;
 		while (newColorTheme === colorTheme) {
